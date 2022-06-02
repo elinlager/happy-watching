24b488a344c8a6b7ee1b44db1f9e8196a2e2ec62
Elin Lager <elinjohannalager@gmail.com>
2022-06-02 10:39:00 +0200

    Add more info to shows in the result list

--- components/ShowLink.vue
+++ components/ShowLink.vue
@@ -1,7 +1,11 @@
 <template>
-  <NuxtLink :to="`/show/${id}`">
+  <NuxtLink :to="`/show/${show.id}`">
     <div class="showcard">
-      <p>{{ name }}</p>
+      <div class="show-info">
+        <h5>{{ show.name }}</h5>
+        <div v-html="show.summary" class="summary" />
+      </div>
+      <img v-if="show.image && show.image.medium" :src="show.image.medium" />
     </div>
   </NuxtLink>
 </template>
@@ -9,36 +13,41 @@
 <script>
 export default {
   props: {
-    name: {
-      type: String,
+    show: {
+      type: Object,
       required: true,
-    },
-    id: {
-      type: Number,
-      required: true
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  a:hover {
-    text-decoration: none;
-  }
-  .showcard {
+a:hover {
+  text-decoration: none;
+}
+.showcard {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background-color: $yellow;
+  border-radius: 0.5rem;
+  color: $black;
+  margin-bottom: 1rem;
+  cursor: pointer;
+  .show-info {
     display: flex;
-    align-items: center;
-    padding: 1rem 2rem;
-    background-color: $yellow;
-    border-radius: 0.5rem;
-    color: $black;
-    margin-bottom: 1rem;
-    cursor: pointer;
-    p {
-      margin: 0;
-    }
-    &:hover {
-      opacity: 0.8;
-    }
+    flex-direction: column;
+    margin-right: 2rem;
   }
+  p {
+    margin: 0;
+  }
+  &:hover {
+    opacity: 0.8;
+  }
+  img {
+    max-height: 100px;
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <div class="search-form">
-      <h3 class="mb-4">Search for a tv show</h3>
+      <h3 class="mb-4">Search for a TV show</h3>
       <div class="d-flex justify-content-center align-items-center">
         <b-form-input v-model.trim="query" type="text" placeholder="The name of a TV-show..."></b-form-input>
         <b-button class="ml-2" @click="search" :disabled="searchButtonDisabled" variant="success">Search</b-button>
       </div>
     </div>
     <div class="show-list">
-      <ShowLink v-for="show in tvshows" :key="show.id" :name="show.name" :id="show.id" />
+      <p v-if="error">Looks like something went wrong. Please try again later!</p>
+      <Spinner v-else-if="loading" />
+      <p v-else-if="tvshows !== null && (tvshows.length < 1)">Could not find any TV show that matches your search. </p>
+      <ShowLink v-for="show in tvshows" :key="show.id" :show="show" />
     </div>
   </div>
 </template>
@@ -20,7 +23,7 @@ export default {
       query: "",
       error: false,
       loading: false,
-      tvshows: []
+      tvshows: null
     }
   },
   computed: {
@@ -37,8 +40,7 @@ export default {
         if (this.query) {
           const response = await fetch(`https://api.tvmaze.com/search/shows?q=${this.query}`)
             .then(resp => resp.json());
-          this.tvshows = response.map(show => ({name: show.show.name, id: show.show.id} ));
-          console.log(this.tvshows)
+          this.tvshows = response.map(show => show.show);
         }
       } catch (error) {
         this.error = true;
